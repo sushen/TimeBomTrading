4ff4be2acfb9917fd539ab861137f5a9dc2d7c45
Sushen Biswas <sushenbiswasaga@gmail.com>
2021-02-15 16:21:16 +0600

    Wall Style Clock

--- script.js
+++ script.js
@@ -3,28 +3,25 @@ const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
 
-var date = new Date();
-console.log(date)
-let hr = date.getHours();
-let min = date.getMinutes();
-let sec = date.getSeconds();
 
-
-let hrPosition = (hr*360)/12 + ((min*360)/60)/12;
-let minPosition = (min*360)/60 + ((sec*360)/60)/60;
-let secPosition = (sec*360)/60;
-
-
-console.log("hours : " +hr);
-console.log("Minit : " + min);
-console.log("Secound : " +sec);
 
 function runTheClock(){
 
-    let hrPosition = hrPosition + (3/360);
-    let minPosition = minPosition + (6/60);
-    let secPosition = secPosition + 6;
+    var date = new Date();
+    console.log(date)
+    let hr = date.getHours();
+    let min = date.getMinutes();
+    let sec = date.getSeconds();
 
+
+    let hrPosition = (hr*360)/12 + ((min*360)/60)/12;
+    let minPosition = (min*360)/60 + ((sec*360)/60)/60;
+    let secPosition = (sec*360)/60;
+
+
+    console.log("hours : " +hr);
+    console.log("Minit : " + min);
+    console.log("Secound : " +sec);
     HOURHAND.style.transform = "rotate(" + hrPosition +"deg)";
     MINUTEHAND.style.transform = "rotate(" + minPosition +"deg)";
     SECONDHAND.style.transform = "rotate(" + secPosition +"deg)";
