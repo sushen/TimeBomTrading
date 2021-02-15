86254c9ea8904fdb843b926d493a0ea7ddfa4fea
Sushen Biswas <sushenbiswasaga@gmail.com>
2021-02-15 15:47:33 +0600

    Giving us the Static Time.

--- script.js
+++ script.js
@@ -2,9 +2,23 @@ const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
-let hrPosition = 100;
-let minPosition = 120;
-let secPosition = 620;
+var date = new Date();
+console.log(date)
+let hr = date.getHours();
+let min = date.getMinutes();
+let sec = date.getSeconds();
+
+
+let hrPosition = hr;
+let minPosition = min;
+let secPosition = sec;
+
+
+
+console.log(hr);
+console.log(min);
+console.log(sec);
+
 
 HOURHAND.style.transform = "rotate(" + hrPosition +"deg)";
 MINUTEHAND.style.transform = "rotate(" + minPosition +"deg)";
